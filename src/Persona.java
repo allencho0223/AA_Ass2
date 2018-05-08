b12fbef457f2dbc9bb6c3cd0a9768fd6228e5046
allencho <s3558745@student.rmit.edu.au>
2018-05-08 11:34:02 +1000

    Modifications and a bit of implementations
    Modifications:
    - Added a method called identifyPersona to check whether the attribute
    (parameter) is valid
    
    Implementations:
    - In RnadomGuessPlay class, I implemented constructor

--- src/Persona.java
+++ src/Persona.java
@@ -1,11 +1,11 @@
 
 public class Persona {
-    
-    private static String name, hairLength, glasses, facialHair, eyeColor, pimples, hat, hairColor, noseShape, faceShape;
-    
-    
 
-    public Persona(String name, String hairLength, String glasses, String facialHair, String eyeColor, String pimples, String hat, String hairColor, String noseShape, String faceShape) {
+    private static String name, hairLength, glasses, facialHair, eyeColor, pimples, hat, hairColor, noseShape,
+            faceShape;
+
+    public Persona(String name, String hairLength, String glasses, String facialHair, String eyeColor, String pimples,
+            String hat, String hairColor, String noseShape, String faceShape) {
         this.name = name;
         this.hairLength = hairLength;
         this.glasses = glasses;
@@ -16,5 +16,33 @@ public class Persona {
         this.hairColor = hairColor;
         this.noseShape = noseShape;
         this.faceShape = faceShape;
+    }
+
+    public String identifyPersona(String att) {
+        switch (att) {
+        case "name":
+            return name;
+        case "hairLength":
+            return hairLength;
+        case "glasses":
+            return glasses;
+        case "facialHair":
+            return facialHair;
+        case "eyeColor":
+            return eyeColor;
+        case "pimples":
+            return pimples;
+        case "hat":
+            return hat;
+        case "hairColor":
+            return hairColor;
+        case "noseShape":
+            return noseShape;
+        case "faceShape":
+            return faceShape;
+        default:
+            return "unknown";
+        }
+
     }
 }
--- src/RandomGuessPlayer.java
+++ src/RandomGuessPlayer.java
@@ -1,39 +1,57 @@
-import java.io.*;
+import java.io.IOException;
+import java.util.Random;
 
 /**
- * Random guessing player.
- * This player is for task B.
+ * Random guessing player. This player is for task B.
  *
  * You may implement/extend other interfaces or classes, but ensure ultimately
  * that this class implements the Player interface (directly or indirectly).
  */
-public class RandomGuessPlayer implements Player
-{
+public class RandomGuessPlayer implements Player {
+
+    public Persona[] personas = null;
+    public Persona chosenPersona = null;
+    public Config config = new Config();
+    public Random r = new Random();
+    
+    public String[] att = new String[] { "hairLength", "glasses", "facialHair", "eyeColor", "pimples", "hat",
+            "hairColor", "noseShape", "faceShape" };
 
     /**
      * Loads the game configuration from gameFilename, and also store the chosen
      * person.
      *
-     * @param gameFilename Filename of game configuration.
-     * @param chosenName Name of the chosen person for this player.
-     * @throws IOException If there are IO issues with loading of gameFilename.
-     *    Note you can handle IOException within the constructor and remove
-     *    the "throws IOException" method specification, but make sure your
-     *    implementation exits gracefully if an IOException is thrown.
+     * @param gameFilename
+     *            Filename of game configuration.
+     * @param chosenName
+     *            Name of the chosen person for this player.
+     * @throws IOException
+     *             If there are IO issues with loading of gameFilename. Note you can
+     *             handle IOException within the constructor and remove the "throws
+     *             IOException" method specification, but make sure your
+     *             implementation exits gracefully if an IOException is thrown.
      */
-    public RandomGuessPlayer(String gameFilename, String chosenName)
-        throws IOException
-    {
+    public RandomGuessPlayer(String gameFilename, String chosenName) throws IOException {
+        // Load and assign data into persona primitive array
+        personas = config.configFileLoader(gameFilename);
+
+        // Ascertain if chosenName exists in the persona list
+        for (int i = 0; i < personas.length; i++) {
+            if ((personas[i].identifyPersona("name").contains(chosenName))) {
+                chosenPersona = personas[i];
+            }
+        }
 
     } // end of RandomGuessPlayer()
 
-
     public Guess guess() {
-
+        String guessAttribute = "";
+        String guessValue = "";
+        
+        
         // placeholder, replace
         return new Guess(Guess.GuessType.Person, "", "Placeholder");
     } // end of guess()
-
 
     public boolean answer(Guess currGuess) {
 
@@ -41,8 +59,7 @@ public class RandomGuessPlayer implements Player
         return false;
     } // end of answer()
 
-
-	public boolean receiveAnswer(Guess currGuess, boolean answer) {
+    public boolean receiveAnswer(Guess currGuess, boolean answer) {
 
         // placeholder, replace
         return true;
