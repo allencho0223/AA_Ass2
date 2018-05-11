ec0ec1019833c357d1617763dd94cffdb2ca8fd9
allencho <s3558745@student.rmit.edu.au>
2018-05-11 11:05:03 +1000

    Working on random guess player
    Need to fix some errors in guess, answer, and receive answer methods,
    Especially sometimes the program throws nullpointerexception in guess
    method. And receive answer method does not display output correctly
    
    java -cp .:jopt-simple-5.0.2.jar GuessWho -l
    ../sampleGameFiles/game1.log ../sampleGameFiles/game1.config
    ../sampleGameFiles/game1.chosen random random
    
    This is the command I’m currently using to test,
    files are in javaSrc folder, and config and chosen files are in
    sampleGameFiles or something like that (different folder)

--- src/Config.java
+++ src/Config.java
@@ -1,12 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class Config {
 
-    public static String[] ATTRIBUTES = new String[] { "hairLength", "glasses", "facial", "eyeColor", "pimples", "hat",
+    public String[] ATTRIBUTES = new String[] { "hairLength", "glasses", "facial", "eyeColor", "pimples", "hat",
             "hairColor", "noseShape", "faceShape" };
 
-    public static List<String> ATTRIBUTE_LIST = Arrays.asList(ATTRIBUTES);
+    public List<String> attributeList = Arrays.asList(ATTRIBUTES);
+
+    public Map<String, ArrayList<String>> attValSet = new HashMap<String, ArrayList<String>>();
+
+    public static void main(String[] args) {
+        Config c = new Config();
+        Random r = new Random();
+        c.configFileLoader("game1.config");
+        String guessAttribute = "";
+        String guessValue = "";
+        ArrayList<String> tempValues = new ArrayList<String>();
+
+        guessAttribute = c.attributeList.get(r.nextInt(c.attributeList.size()));
+
+        for (String value : c.attValSet.get(guessAttribute)) {
+            tempValues.add(value);
+        }
+
+        guessValue = c.attValSet.get(guessAttribute).get(r.nextInt(tempValues.size()));
+
+        System.out.println(guessAttribute);
+        System.out.println(guessValue);
+
+        
+    }
 
     public Persona[] configFileLoader(String gameFileName) {
 
@@ -32,18 +63,27 @@ public class Config {
 
             persona = new Persona[playerNum];
 
-            // Reset BufferedReader object for reuse
-
             playerNum = 0;
 
             // Skip attribute example lines
             for (int i = 0; i < 9; i++) {
-                configData = valueReader.readLine();
+                String[] tempString = new String[10];
+                ArrayList<String> valueList = new ArrayList<String>();
+
+                tempString = valueReader.readLine().split("\\s");
+                for (int j = 1; j < tempString.length; j++) {
+                    valueList.add(tempString[j]);
+                }
+                attValSet.put(tempString[0], valueList);
+            }
+
+            for (Map.Entry<String, ArrayList<String>> entry : attValSet.entrySet()) {
+                System.out.println(entry.getKey() + ":" + entry.getValue().toString());
             }
 
             while ((configData = valueReader.readLine()) != null) {
-                String[] attribute = new String[2];
 
+                String[] attribute = new String[2];
                 /**
                  * value array: [0]: hairLength [1]: glasses [2]: facialHair [3]: eyeColor [4]:
                  * pimples [5]: hat [6]: hairColor [7]: noseShape [8]: faceShape
@@ -51,15 +91,17 @@ public class Config {
                 String[] value = new String[9];
 
                 String name = valueReader.readLine();
-
+                if (name == null) {
+                    break;
+                }
                 for (int i = 0; i < value.length; i++) {
                     attribute = valueReader.readLine().split("\\s");
                     value[i] = attribute[1];
                 }
-
                 // Generate personas based on the number of written data of config file
                 persona[playerNum++] = new Persona(name, value[0], value[1], value[2], value[3], value[4], value[5],
                         value[6], value[7], value[8]);
+
             }
 
         } catch (IOException ioe) {
--- src/RandomGuessPlayer.java
+++ src/RandomGuessPlayer.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -10,12 +13,11 @@ import java.util.Random;
 public class RandomGuessPlayer implements Player {
 
     public Persona[] personas = null;
+    public List<Persona> personaList;
     public Persona chosenPersona = null;
     public Config config = new Config();
     public Random r = new Random();
-    
-    public String[] att = new String[] { "hairLength", "glasses", "facialHair", "eyeColor", "pimples", "hat",
-            "hairColor", "noseShape", "faceShape" };
+    public int alivePersona = 0;
 
     /**
      * Loads the game configuration from gameFilename, and also store the chosen
@@ -42,27 +44,101 @@ public class RandomGuessPlayer implements Player {
             }
         }
 
+        personaList = new ArrayList<Persona>(Arrays.asList(personas));
+
+        alivePersona = personas.length;
     } // end of RandomGuessPlayer()
 
     public Guess guess() {
+
+        // Make a guess
         String guessAttribute = "";
         String guessValue = "";
-        
-        
-        // placeholder, replace
-        return new Guess(Guess.GuessType.Person, "", "Placeholder");
+        ArrayList<String> tempValues = new ArrayList<String>();
+        ArrayList<Guess.GuessType> tempGuessType = new ArrayList<Guess.GuessType>();
+        Guess.GuessType guessType = null;
+
+        /**
+         * Value. if mType = Attribute, then it is the value of the associated
+         * attribute). if mType = Person, then this is the guessed person's name.
+         */
+
+        for (Guess.GuessType type : guessType.values()) {
+            tempGuessType.add(type);
+        }
+
+        guessType = tempGuessType.get(r.nextInt(tempGuessType.size()));
+
+        /**
+         * If guess type is person: att == "" value == person's name
+         */
+        if (guessType == Guess.GuessType.Person) {
+
+            guessAttribute = "";
+
+            guessValue = personas[r.nextInt(personas.length)].name;
+
+        } else {
+            /**
+             * Else - guess type is attribute: attribute == the attribute of persona value
+             * == value corresponding to the attribute
+             */
+            guessAttribute = config.attributeList.get(r.nextInt(config.attributeList.size()));
+
+            for (String value : config.attValSet.get(guessAttribute)) {
+                tempValues.add(value);
+            }
+
+            guessValue = config.attValSet.get(guessAttribute).get(r.nextInt(tempValues.size()));
+
+        }
+
+        return new Guess(guessType, guessAttribute, guessValue);
+
     } // end of guess()
 
     public boolean answer(Guess currGuess) {
 
-        // placeholder, replace
-        return false;
+        String chosenValue = "";
+        int isRemoved = 0;
+        // If mType == Person
+        if (currGuess.getType() == Guess.GuessType.Person) {
+
+            // Compare value with chosen person's name
+            if (chosenPersona.name == currGuess.getValue()) {
+                return true;
+            }
+            return false;
+
+            // Else - mType == Attribute
+        } else {
+
+            // Check how many personas have the value
+            for (int i = 0; i < personas.length; i++) {
+                // Check how many personas have the value corresponding to the attribute
+                chosenValue = personas[i].identifyPersona(currGuess.getAttribute());
+                if (chosenValue == currGuess.getValue()) {
+                    personaList.remove(i--);
+                    alivePersona--;
+                    isRemoved++;
+                }
+            }
+            if (isRemoved > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     } // end of answer()
 
     public boolean receiveAnswer(Guess currGuess, boolean answer) {
-
-        // placeholder, replace
-        return true;
+        
+        if (currGuess.getValue() == chosenPersona.name) {
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     } // end of receiveAnswer()
 
 } // end of class RandomGuessPlayer
